6a1e91a5cfeb3df991c2f1ba2f240c1e26243adb
Michael Schout <mschout@gkg.net>
2010-03-17 13:09:07 -0500

    use Dist::Zilla - raaaaaaaar!

--- lib/Template/Plugin/Filter/Minify/CSS.pm
+++ lib/Template/Plugin/Filter/Minify/CSS.pm
@@ -5,8 +5,6 @@ use strict;
 use base 'Template::Plugin::Filter';
 use CSS::Minifier;
 
-our $VERSION = '0.92';
-
 sub init {
     my $self = shift;
 
@@ -25,9 +23,7 @@ sub filter {
 
 1;
 
-=head1 NAME
-
-Template::Plugin::Filter::Minify::CSS - CSS::Minifier filter for Template Toolkit
+# ABSTRACT: CSS::Minifier filter for Template Toolkit
 
 =head1 SYNOPSIS
 
@@ -44,44 +40,6 @@ Template::Plugin::Filter::Minify::CSS - CSS::Minifier filter for Template Toolki
 
 This module is a Template Toolkit filter, which uses CSS::Minifier to minify
 css code from filtered content during template processing.
-
-=head1 SOURCE
-
-You can contribute or fork this project via github:
-
-http://github.com/mschout/template-plugin-filter-minify-css
-
- git clone git://github.com/mschout/template-plugin-filter-minify-css.git
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-bug-template-plugin-filter-minify-css@rt.cpan.org, or through the web
-interface at http://rt.cpan.org/
-
-=head1 AUTHOR
-
-Michael Schout E<lt>mschout@cpan.orgE<gt>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Michael Schout.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of either:
-
-=over 4
-
-=item *
-
-the GNU General Public License as published by the Free Software Foundation;
-either version 1, or (at your option) any later version, or
-
-=item *
-
-the Artistic License version 2.0.
-
-=back
 
 =head1 SEE ALSO
 
