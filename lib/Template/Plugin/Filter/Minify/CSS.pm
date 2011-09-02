30cf1ec4f2e9577a8386cc32ae780541f6835c59
Michael Schout <mschout@gkg.net>
2011-09-02 17:10:42 -0500

    Build results of 45e04bf (on master)

--- lib/Template/Plugin/Filter/Minify/CSS.pm
+++ lib/Template/Plugin/Filter/Minify/CSS.pm
@@ -1,4 +1,7 @@
 package Template::Plugin::Filter::Minify::CSS;
+BEGIN {
+  $Template::Plugin::Filter::Minify::CSS::VERSION = '0.94';
+}
 # ABSTRACT: CSS::Minifier filter for Template Toolkit
 
 use 5.006;
@@ -6,14 +9,6 @@ use strict;
 use base 'Template::Plugin::Filter';
 use CSS::Minifier;
 
-=begin Pod::Coverage
-
-init
-filter
-
-=end Pod::Coverage
-
-=cut
 
 sub init {
     my $self = shift;
@@ -33,6 +28,18 @@ sub filter {
 
 1;
 
+
+__END__
+=pod
+
+=head1 NAME
+
+Template::Plugin::Filter::Minify::CSS - CSS::Minifier filter for Template Toolkit
+
+=head1 VERSION
+
+version 0.94
+
 =head1 SYNOPSIS
 
   [% USE Filter.Minify.CSS %]
@@ -49,7 +56,33 @@ sub filter {
 This module is a Template Toolkit filter, which uses CSS::Minifier to minify
 css code from filtered content during template processing.
 
+=for Pod::Coverage init
+filter
+
 =head1 SEE ALSO
 
 L<CSS::Minifier>, L<Template::Plugin::Filter>, L<Template>
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/mschout/template-plugin-filter-minify-css>
+and may be cloned from L<git://github.com/mschout/template-plugin-filter-minify-css.git>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to bug-template-plugin-filter-minify-css@rt.cpan.org or through the web interface at:
+ http://rt.cpan.org/Public/Dist/Display.html?Name=Template-Plugin-Filter-Minify-CSS
+
+=head1 AUTHOR
+
+Michael Schout <mschout@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Michael Schout.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
 
